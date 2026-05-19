2883ba3a9a8b14786be7784fcaf1cde5217aad29
Ming Chen <chenming202503@163.com>
2026-05-19 14:48:19 +0800

    feat(web): add lint, format, test, git hooks toolchain

---
+++ yixiang-web/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': 'warn',
+  },
+  ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
+};
