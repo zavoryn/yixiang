541c04e25e447447afcc04e25956213633e24d08
Ming Chen <chenming202503@163.com>
2026-05-20 16:33:50 +0800

    refactor(be): rename ZhiGuangApplication → YiXiangApplication
    Align main class name with project name. Update pom.xml mainClass reference.

--- yixiang_be/src/main/java/com/tongji/YiXiangApplication.java
+++ yixiang_be/src/main/java/com/tongji/YiXiangApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZhiGuangApplication {
+public class YiXiangApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZhiGuangApplication.class, args);
+        SpringApplication.run(YiXiangApplication.class, args);
     }
 }
 
